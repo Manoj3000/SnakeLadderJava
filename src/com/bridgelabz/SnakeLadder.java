8446b5f9854b23dd88ed340378228f8ff27dd8c5
manoj gaikwad <gaikwadm853@gmail.com>
2021-09-15 00:41:43 +0530

    Added: player start position 0

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,5 +4,8 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder program");
+		int startPosition = 0;
+
+		System.out.println("start position of player : " + startPosition);
 	}
 }
